1856847d44d75087b9b5d2cb496542d5a8e10b63
neksus-hub <nik.dubinin.01@mail.ru>
2025-10-10 16:45:47 +0300

    get_data

---
+++ js/getGoods.js
@@ -0,0 +1,22 @@
+const getGoods = () => {
+    const links = document.querySelectorAll('.navigation-link')
+
+    const getData = () => {
+        fetch('/db/db.json')
+            .then((res) => res.json())
+            .then((data) => localStorage.setItem('data', JSON.stringify(data)))
+    }
+
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+            getData()
+        })
+    })
+
+    localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]))
+
+
+}
+
+getGoods()
